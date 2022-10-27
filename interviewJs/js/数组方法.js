5cd583fdf3c27622f6cd7e2411437333f0bc57d0
Lforwhat <1277149488@qq.com>
2022-10-28 00:19:34 +0800

    upload the arr method

---
+++ interviewJs/js/数组方法.js
@@ -0,0 +1,120 @@
+const player = [
+    { name: "科比", count: 0},
+    { name: "布兰特", count: 10 },
+    { name: "詹姆斯", count: 2 },
+    { name: "保罗", count: 15 },
+    { name: "姚明", count: 24 },
+]
+
+
+// forEach method to iterate each item
+Array.prototype.sl_forEach = function (callBack) {
+    for (let i = 0; i < this.length; i++) {
+        callBack(this[i], i, this);
+    }
+};
+
+player.sl_forEach((item, index) => {
+    console.log(item, index);
+});
+
+// map method will return a new Array
+Array.prototype.sl_map = function (callBack) {
+    const res = [];
+    for (let i = 0; i < this.length; i++) {
+        res.push(callBack(this[i], i, this));
+    }
+    return res;
+};
+
+console.log(player.sl_map((item, index) => {
+    return `${item.name} - ${item.count} - ${index}`;
+}));
+
+// filter method will return the item that the callback return true
+Array.prototype.sl_filter = function (callBack) {
+    const res = [];
+    for (let i = 0; i < this.length; i++) {
+        callBack(this[i], i, this) && res.push(callBack(this[i], i, this));
+    }
+    return res;
+};
+
+console.log(player.sl_filter((item) => {
+    if (item.count > 10) return item;
+}));
+
+
+// every method： test whether each element satisfy the callback fucntion
+Array.prototype.sl_every = function (callBack) {
+    let flag = true;
+    for (let i = 0; i < this.length; i++) {
+        flag = callBack(this[i], i, this);
+        if (!flag) break;
+    }
+    return flag;
+};
+
+
+console.log(player.sl_every((item) => {
+    return item.count >= 10;
+}));
+
+
+// some method: test whether there are one element to satisfy the callback
+Array.prototype.sl_some = function (callBack) {
+    let flag = false;
+    for (let i = 0; i < this.length; i++) {
+        flag = callBack(this[i], i, this);
+        if (flag) break;
+    }
+    return flag;
+};
+
+console.log(player.some((item, index) => {
+    return item.count >= 10;
+}));
+
+
+// reduce method calculate the result of the array.
+Array.prototype.sl_reduce = function (callBack, ...args) {
+    let start = 0, pre = 0;
+    if (args.length) {
+        pre = args[0];
+    } else {
+        pre = this[0].count;
+        start = 1;
+    }
+    for (let i = start; i < this.length; i++) {
+        pre = callBack(pre, this[i], i, this);
+    }
+    return pre;
+};
+
+console.log(player.sl_reduce((prev, item) => {
+    return prev + item.count;
+}));
+
+
+
+// findIndex  
+Array.prototype.sl_findIndex = function (callBack) {
+    for (let i = 0; i < this.length; i++) {
+        if (callBack(this[i], i, this)) {
+            return i;
+        }
+    }
+    return -1;
+};
+
+console.log(player.sl_findIndex((item) => item.name === "姚明"));
+
+
+
+
+
+
+
+
+
+
